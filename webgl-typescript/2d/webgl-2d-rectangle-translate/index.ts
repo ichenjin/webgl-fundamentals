8d6c8d08691dac6e40616d837232618d6adc09b9
陈津 <ichenjin@hotmail.com>
2016-11-01 16:33:43 +0800

    complete 2d scale

---
+++ webgl-typescript/2d/webgl-2d-rectangle-translate/index.ts
@@ -0,0 +1,113 @@
+﻿namespace webgl_2d_rectangle_translate {
+
+    function main() {
+        // Get A WebGL context
+        /** @type {HTMLCanvasElement} */
+        let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+        webglLessonsHelper.setupLesson(canvas);
+        let gl = canvas.getContext("webgl");
+        if (!gl) {
+            webglLessonsHelper.showNeedWebGL(canvas);
+            return;
+        }
+
+        // setup GLSL program
+        let program = webglUtils.createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
+
+        // look up where the vertex data needs to go.
+        let positionLocation = gl.getAttribLocation(program, "a_position");
+
+        // lookup uniforms
+        let resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+        let colorLocation = gl.getUniformLocation(program, "u_color");
+
+        // Create a buffer to put positions in
+        let positionBuffer = gl.createBuffer();
+
+        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        let translation = [0, 0];
+        let width = 100;
+        let height = 30;
+        let color = [Math.random(), Math.random(), Math.random(), 1];
+
+        drawScene();
+
+        // Setup a ui.
+        webglLessonsHelper.setupSlider("#x", { slide: updatePosition(0), max: gl.canvas.width });
+        webglLessonsHelper.setupSlider("#y", { slide: updatePosition(1), max: gl.canvas.height });
+
+        function updatePosition(index) {
+            return function (event, ui) {
+                translation[index] = ui.value;
+                drawScene();
+            }
+        }
+
+        // Draw a the scene.
+        function drawScene() {
+            webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+            // Tell WebGL how to convert from clip space to pixels
+            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+            // Clear the canvas.
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            // Tell it to use our program (pair of shaders)
+            gl.useProgram(program);
+
+            // Turn on the attribute
+            gl.enableVertexAttribArray(positionLocation);
+
+            // Bind the position buffer.
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+            // Setup a rectangle
+            setRectangle(gl, translation[0], translation[1], width, height);
+
+            // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+            let size = 2;          // 2 components per iteration
+            let type = gl.FLOAT;   // the data is 32bit floats
+            let normalize = false; // don't normalize the data
+            let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+            let offset = 0;        // start at the beginning of the buffer
+            gl.vertexAttribPointer(
+                positionLocation, size, type, normalize, stride, offset)
+
+            // set the resolution
+            gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+            // set the color
+            gl.uniform4fv(colorLocation, color);
+
+            // Draw the rectangle.
+            let primitiveType = gl.TRIANGLES;
+            offset = 0;
+            let count = 6;
+            gl.drawArrays(primitiveType, offset, count);
+        }
+    }
+
+    // Fill the buffer with the values that define a rectangle.
+    function setRectangle(gl: WebGLRenderingContext, x: number, y: number, width: number, height: number) {
+        let x1 = x;
+        let x2 = x + width;
+        let y1 = y;
+        let y2 = y + height;
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array([
+                x1, y1,
+                x2, y1,
+                x1, y2,
+                x1, y2,
+                x2, y1,
+                x2, y2,
+            ]),
+            gl.STATIC_DRAW);
+    }
+
+    main();
+}
---
+++ webgl-typescript/index.d.ts
@@ -0,0 +1,39 @@
+﻿/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+
+declare interface webglUtils {
+    /**
+     * Creates a program from 2 script tags.
+     *
+     * @param {WebGLRenderingContext} gl The WebGLRenderingContext
+     *        to use.
+     * @param {string[]} shaderScriptIds Array of ids of the script
+     *        tags for the shaders. The first is assumed to be the
+     *        vertex shader, the second the fragment shader.
+     * @param {string[]} [opt_attribs] An array of attribs names. Locations will be assigned by index if not passed in
+     * @param {number[]} [opt_locations] The locations for the. A parallel array to opt_attribs letting you assign locations.
+     * @param {module:webgl-utils.ErrorCallback} opt_errorCallback callback for errors. By default it just prints an error to the console
+     *        on error. If you want something else pass an callback. It's passed an error message.
+     * @return {WebGLProgram} The created program.
+     * @memberOf module:webgl-utils
+     */
+    createProgramFromScripts(gl: WebGLRenderingContext, shaderScriptIds: Array<string>,
+        opt_attribs?: any, opt_locations?: any, opt_errorCallback?: Function): WebGLProgram;
+
+    /**
+     * Resize a canvas to match the size its displayed.
+     * @param {HTMLCanvasElement} canvas The canvas to resize.
+     * @param {number} [multiplier] amount to multiply by.
+     *    Pass in window.devicePixelRatio for native pixels.
+     * @return {boolean} true if the canvas was resized.
+     * @memberOf module:webgl-utils
+     */
+    resizeCanvasToDisplaySize(canvas: HTMLCanvasElement, multiplier?: number): void;
+}
+declare const webglUtils: webglUtils;
+
+declare interface webglLessonsHelper {
+    showNeedWebGL(canvas: HTMLCanvasElement): void;
+    setupLesson(canvas: HTMLCanvasElement, opt_options?: any): void;
+    setupSlider(selector: string, options?: any): void;
+}
+declare const webglLessonsHelper: webglLessonsHelper;
